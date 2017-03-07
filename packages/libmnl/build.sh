3462752f14f3ed7ad9b130f99bfd6145777d998b
vishalbiswas <vshlbiswas@ymail.com>
2017-03-07 08:51:42 +0530

    libmnl: add package

---
+++ packages/libmnl/build.sh
@@ -0,0 +1,7 @@
+TERMUX_PKG_HOMEPAGE=http://www.netfilter.org/projects/libmnl/
+TERMUX_PKG_VERSION=1.0.4
+TERMUX_PKG_SRCURL=http://www.netfilter.org/projects/libmnl/files/libmnl-$TERMUX_PKG_VERSION.tar.bz2
+TERMUX_PKG_SHA256=171f89699f286a5854b72b91d06e8f8e3683064c5901fb09d954a9ab6f551f81
+TERMUX_PKG_MAINTAINER="Vishal Biswas @vishalbiswas"
+TERMUX_PKG_DESCRIPTION="minimalistic Netlink communication library"
+
