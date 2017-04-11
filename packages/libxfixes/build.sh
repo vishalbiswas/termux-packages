927b7a03a6424f7701d8a30b30bd12837f8a147b
vishalbiswas <vshlbiswas@ymail.com>
2017-04-11 18:05:54 +0530

    libxfixes: add package

---
+++ packages/libxfixes/build.sh
@@ -0,0 +1,5 @@
+TERMUX_PKG_VERSION=5.0.3
+TERMUX_PKG_SRCURL=https://www.x.org/archive/individual/lib/libXfixes-$TERMUX_PKG_VERSION.tar.bz2
+TERMUX_PKG_DEPENDS="libx11, x11-proto"
+TERMUX_PKG_SHA256=de1cd33aff226e08cefd0e6759341c2c8e8c9faf8ce9ac6ec38d43e287b22ad6
+TERMUX_PKG_HOMEPAGE=https://www.x.org
