3dade21009071bf86aaf2c572cdd0c7221d3d475
vishalbiswas <vshlbiswas@ymail.com>
2017-05-03 13:32:35 +0530

    libx11: enable more features

--- packages/libx11/build.sh
+++ packages/libx11/build.sh
@@ -1,14 +1,8 @@
 TERMUX_PKG_VERSION=1.6.4
 TERMUX_PKG_SRCURL=https://www.x.org/archive//individual/lib/libX11-$TERMUX_PKG_VERSION.tar.bz2
-TERMUX_PKG_DEPENDS="x11-proto, xtrans, libxcb"
+TERMUX_PKG_DEPENDS="x11-proto, xtrans, libxcb, libxkbfile, libandroid-support"
 TERMUX_PKG_SHA256=b7c748be3aa16ec2cbd81edc847e9b6ee03f88143ab270fb59f58a044d34e441
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-xthreads
---disable-xcms
---disable-xlocale
---disable-xf86bigfont
---disable-xkb
---disable-composecache
---disable-loadable-xcursor
 --enable-malloc0returnsnull
 "
