aecf8bbd258e1541e4a434a2a4c317dee87436a6
vishalbiswas <vshlbiswas@ymail.com>
2017-05-11 10:07:05 +0530

    libxaw: add package

---
+++ packages/libxaw/build.sh
@@ -0,0 +1,5 @@
+TERMUX_PKG_HOMEPAGE=https://www.x.org
+TERMUX_PKG_VERSION=1.0.13
+TERMUX_PKG_SRCURL=https://www.x.org/archive//individual/lib/libXaw-$TERMUX_PKG_VERSION.tar.bz2
+TERMUX_PKG_SHA256=8ef8067312571292ccc2bbe94c41109dcf022ea5a4ec71656a83d8cce9edb0cd
+TERMUX_PKG_DEPENDS="libxmu, libxpm"
