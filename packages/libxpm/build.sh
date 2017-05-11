ad57155f896adaf6a5ab10d5cd19ef8007f8010e
vishalbiswas <vshlbiswas@ymail.com>
2017-05-11 10:08:02 +0530

    libxpm: add package

---
+++ packages/libxpm/build.sh
@@ -0,0 +1,5 @@
+TERMUX_PKG_HOMEPAGE=https://www.x.org
+TERMUX_PKG_VERSION=3.5.12
+TERMUX_PKG_SRCURL=https://www.x.org/archive//individual/lib/libXpm-$TERMUX_PKG_VERSION.tar.bz2
+TERMUX_PKG_SHA256=fd6a6de3da48de8d1bb738ab6be4ad67f7cb0986c39bd3f7d51dd24f7854bdec
+TERMUX_PKG_DEPENDS="libxt"
