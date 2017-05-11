904b848e17a25685bc3236b0f03979c0241fd4bd
vishalbiswas <vshlbiswas@ymail.com>
2017-05-11 10:05:01 +0530

    xclock: add package

---
+++ packages/xclock/build.sh
@@ -0,0 +1,5 @@
+TERMUX_PKG_HOMEPAGE=https://www.x.org
+TERMUX_PKG_VERSION=1.0.7
+TERMUX_PKG_SRCURL=https://www.x.org/archive//individual/app/xclock-$TERMUX_PKG_VERSION.tar.bz2
+TERMUX_PKG_SHA256=23ceeca94e3e20a6c26a703ac7f789066d4517f8d2cb717ae7cb28a617d97dd0
+TERMUX_PKG_DEPENDS="libx11, libxmu, libxaw, libxkbfile, libxrender, libxft, libandroid-support"
