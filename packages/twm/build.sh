791a94648ae5addb2d10857642ede7e65e193649
vishalbiswas <vshlbiswas@ymail.com>
2017-05-11 10:05:39 +0530

    twm: add package

---
+++ packages/twm/build.sh
@@ -0,0 +1,5 @@
+TERMUX_PKG_HOMEPAGE=https://www.x.org
+TERMUX_PKG_VERSION=1.0.9
+TERMUX_PKG_SRCURL=https://www.x.org/archive//individual/app/twm-$TERMUX_PKG_VERSION.tar.bz2
+TERMUX_PKG_SHA256=c575167ac33edf3bd1592f1bedfe0559debb128ed5fb4873c1278e3be166310a
+TERMUX_PKG_DEPENDS="libxmu"
