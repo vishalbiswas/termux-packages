e5dccb202bd68119850acae4b8772beaa2b6dc04
vishalbiswas <vshlbiswas@ymail.com>
2017-05-11 10:07:32 +0530

    libxft: add package

---
+++ packages/libxft/build.sh
@@ -0,0 +1,5 @@
+TERMUX_PKG_HOMEPAGE=https://www.x.org
+TERMUX_PKG_VERSION=2.3.2
+TERMUX_PKG_SRCURL=https://www.x.org/archive//individual/lib/libXft-$TERMUX_PKG_VERSION.tar.bz2
+TERMUX_PKG_SHA256=f5a3c824761df351ca91827ac221090943ef28b248573486050de89f4bfcdc4c
+TERMUX_PKG_DEPENDS="libxrender, fontconfig"
