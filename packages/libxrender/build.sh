fdc5a6c8fefab61041b97ae320bedd2118409bc4
vishalbiswas <vshlbiswas@ymail.com>
2017-05-11 10:06:47 +0530

    librender: add package

---
+++ packages/libxrender/build.sh
@@ -0,0 +1,5 @@
+TERMUX_PKG_HOMEPAGE=https://www.x.org
+TERMUX_PKG_VERSION=0.9.10
+TERMUX_PKG_SRCURL=https://www.x.org/archive//individual/lib/libXrender-$TERMUX_PKG_VERSION.tar.bz2
+TERMUX_PKG_SHA256=c06d5979f86e64cabbde57c223938db0b939dff49fdb5a793a1d3d0396650949
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-malloc0returnsnull"
