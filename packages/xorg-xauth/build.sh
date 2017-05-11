cae4e3775705c24f13340d25860cf964ab936258
vishalbiswas <vshlbiswas@ymail.com>
2017-05-11 10:09:25 +0530

    xorg-xauth: add package

---
+++ packages/xorg-xauth/build.sh
@@ -0,0 +1,5 @@
+TERMUX_PKG_HOMEPAGE=https://xorg.freedesktop.org
+TERMUX_PKG_VERSION=1.0.10
+TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/releases/individual/app/xauth-$TERMUX_PKG_VERSION.tar.bz2
+TERMUX_PKG_SHA256=5afe42ce3cdf4f60520d1658d2b17face45c74050f39af45dccdc95e73fafc4d
+TERMUX_PKG_DEPENDS="libxext, libxmu"
