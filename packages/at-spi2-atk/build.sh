96d32e5f6592ef9e0a9cf1c40605ab84d24a8aca
vishalbiswas <vshlbiswas@ymail.com>
2017-05-25 07:19:48 +0530

    at-spi2-atk: add package

---
+++ packages/at-spi2-atk/build.sh
@@ -0,0 +1,7 @@
+TERMUX_PKG_HOMEPAGE=https://wiki.gnome.org/Accessibility
+_major=2.24
+_minor=1
+TERMUX_PKG_VERSION=$_major.$_minor
+TERMUX_PKG_SRCURL=https://ftp.gnome.org/pub/gnome/sources/at-spi2-atk/$_major/at-spi2-atk-$TERMUX_PKG_VERSION.tar.xz
+TERMUX_PKG_SHA256=60dc90ac4f74b8ffe96a9363c25208a443b381bacecfefea6de549f20ed6957d
+TERMUX_PKG_DEPENDS="libatk, libx11, libdbus"
