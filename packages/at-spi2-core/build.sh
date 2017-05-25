8c0b75b6630f46a654de68906e6a3f64dc436508
vishalbiswas <vshlbiswas@ymail.com>
2017-05-25 07:19:56 +0530

    at-spi2-core: add package

---
+++ packages/at-spi2-core/build.sh
@@ -0,0 +1,7 @@
+TERMUX_PKG_HOMEPAGE=http://www.gnome.org
+_major=2.24
+_minor=1
+TERMUX_PKG_VERSION=$_major.$_minor
+TERMUX_PKG_SRCURL=http://ftp.gnome.org/pub/gnome/sources/at-spi2-core/$_major/at-spi2-core-$TERMUX_PKG_VERSION.tar.xz
+TERMUX_PKG_SHA256=1e90d064b937aacfe79a96232ac7e63d28d716e85bd9ff4333f865305a959b5b
+TERMUX_PKG_DEPENDS="libdbus, glib, libxtst"
