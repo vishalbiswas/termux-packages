7767f1f7d4de84439f71b81e5ae4784e2056d087
vishalbiswas <vshlbiswas@ymail.com>
2017-05-25 07:23:17 +0530

    libxtst: add package

---
+++ packages/libxtst/build.sh
@@ -0,0 +1,5 @@
+TERMUX_PKG_HOMEPAGE=https://www.x.org
+TERMUX_PKG_VERSION=1.2.3
+TERMUX_PKG_SRCURL=https://www.x.org/archive//individual/lib/libXtst-$TERMUX_PKG_VERSION.tar.bz2
+TERMUX_PKG_SHA256=4655498a1b8e844e3d6f21f3b2c4e2b571effb5fd83199d428a6ba7ea4bf5204
+TERMUX_PKG_DEPENDS="libxext, libxi, x11-proto, libxfixes"
