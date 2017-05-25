fe81686b83d57ac31a96d4db390a9caf64ed09a7
vishalbiswas <vshlbiswas@ymail.com>
2017-05-25 07:20:49 +0530

    libatk: add package

---
+++ packages/libatk/build.sh
@@ -0,0 +1,7 @@
+TERMUX_PKG_HOMEPAGE=https://www.gtk.org
+_major=2.24
+_minor=0
+TERMUX_PKG_VERSION=$_major.$_minor
+TERMUX_PKG_SRCURL=http://ftp.gnome.org/pub/gnome/sources/atk/$_major/atk-$TERMUX_PKG_VERSION.tar.xz
+TERMUX_PKG_SHA256=bb2daa9a808c73a7a79d2983f333e0ba74be42fc51e3ba1faf2551a636487a49
+TERMUX_PKG_DEPENDS="glib, libandroid-support"
