2b07aeed4be291aa3347213951202e5aa8b2425c
vishalbiswas <vshlbiswas@ymail.com>
2017-05-25 07:21:19 +0530

    libdrm: add package

---
+++ packages/libdrm/build.sh
@@ -0,0 +1,10 @@
+TERMUX_PKG_HOMEPAGE=http://dri.freedesktop.org
+TERMUX_PKG_VERSION=2.4.80
+TERMUX_PKG_SRCURL=https://dri.freedesktop.org/libdrm/libdrm-$TERMUX_PKG_VERSION.tar.bz2
+TERMUX_PKG_SHA256=a82a519601e9cdfad795e760807eb07ac8913b225e25fc8fe9bc03e3be6549f1
+#TERMUX_PKG_EXTRA_CONFIGURE_ARGS=
+TERMUX_PKG_CLANG=no
+
+termux_step_pre_configure () {
+	CFLAGS="$CFLAGS -DANDROID"
+}
