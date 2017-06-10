7f30a7a63ecd3914d9409a61e3921edac3ceb8a1
vishalbiswas <vshlbiswas@ymail.com>
2017-06-10 15:54:45 +0530

    libepoxy: enable glx

--- packages/libepoxy/build.sh
+++ packages/libepoxy/build.sh
@@ -1,7 +1,9 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/anholt/libepoxy
 TERMUX_PKG_VERSION=1.4.0
 TERMUX_PKG_SRCURL=https://github.com/anholt/libepoxy/releases/download/v${TERMUX_PKG_VERSION%.0}/libepoxy-$TERMUX_PKG_VERSION.tar.xz
 TERMUX_PKG_SHA256=25a906b14a921bc2b488cfeaa21a00486fe92630e4a9dd346e4ecabeae52ab41
 TERMUX_PKG_DESCRIPTION="Library handling OpenGL function pointer management"
+#TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-glx"
 
 termux_step_pre_configure () {
 	export EGL_CFLAGS=$CFLAGS
