ed9fbe3663ca1b1c6574aef8f10d7f71bced9fa3
vishalbiswas <vshlbiswas@ymail.com>
2017-06-10 15:50:33 +0530

    at-spi2-core: set proper dbus dir

--- packages/at-spi2-core/build.sh
+++ packages/at-spi2-core/build.sh
@@ -5,3 +5,4 @@ TERMUX_PKG_VERSION=$_major.$_minor
 TERMUX_PKG_SRCURL=http://ftp.gnome.org/pub/gnome/sources/at-spi2-core/$_major/at-spi2-core-$TERMUX_PKG_VERSION.tar.xz
 TERMUX_PKG_SHA256=1e90d064b937aacfe79a96232ac7e63d28d716e85bd9ff4333f865305a959b5b
 TERMUX_PKG_DEPENDS="libdbus, glib, libxtst"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-dbus-daemondir=$TERMUX_PREFIX/bin"
