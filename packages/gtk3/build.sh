4ae0f72b1c8f0b0c94aa984165dd5c17ebbdb990
vishalbiswas <vshlbiswas@ymail.com>
2017-06-10 15:51:53 +0530

    gtk3: add dependency on adwaita-icon-theme

--- packages/gtk3/build.sh
+++ packages/gtk3/build.sh
@@ -4,5 +4,5 @@ _minor=12
 TERMUX_PKG_VERSION=$_major.$_minor
 TERMUX_PKG_SRCURL=http://ftp.gnome.org/pub/gnome/sources/gtk+/$_major/gtk+-$TERMUX_PKG_VERSION.tar.xz
 TERMUX_PKG_SHA256=84fae0cefb6a11ee2b4e86b8ac42fe46a3d30b4ad16661d5fc51e8ae03e2a98c
-TERMUX_PKG_DEPENDS="libcairo, libcairo-gobject, libxrender, libepoxy, gdk-pixbuf, pango, librsvg, libatk, libxi, fontconfig, libx11, libxext, libxfixes"
+TERMUX_PKG_DEPENDS="libcairo, libcairo-gobject, libxrender, libepoxy, gdk-pixbuf, pango, librsvg, libatk, libxi, fontconfig, libx11, libxext, libxfixes, adwaita-icon-theme"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-gtk-doc-html --disable-cups --enable-x11-backend --disable-glibtest"
