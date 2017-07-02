fe5ce15d792d4fd0681ba335b21a1523d686c5ca
vishalbiswas <vshlbiswas@ymail.com>
2017-07-02 22:53:26 +0530

    xterm: update from 327 to 330

--- packages/xterm/build.sh
+++ packages/xterm/build.sh
@@ -1,10 +1,18 @@
 TERMUX_PKG_HOMEPAGE=http://invisible-island.net/xterm
-TERMUX_PKG_VERSION=327
+TERMUX_PKG_VERSION=330
 TERMUX_PKG_SRCURL=ftp://invisible-island.net/xterm/xterm-$TERMUX_PKG_VERSION.tgz
-TERMUX_PKG_SHA256=66fb2f6c35b342148f549c276b12a3aa3fb408e27ab6360ddec513e14376150b
+TERMUX_PKG_SHA256=7aeef9f29f6b95e09f481173c8c3053357bf5ffe162585647f690fd1707556df
 TERMUX_PKG_DEPENDS="libxaw, ncurses, libxkbfile"
 TERMUX_PKG_MAINTAINER="Vishal Biswas @vishalbiswas"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="cf_cv_have_utmp=no ac_cv_header_pty_h=no ac_cv_func_grantpt=yes --disable-openpty --disable-pty-handshake --disable-setuid --disable-setgid --disable-luit"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+cf_cv_have_utmp=no
+ac_cv_header_pty_h=no
+ac_cv_func_grantpt=yes
+--disable-openpty
+--disable-pty-handshake
+--disable-setuid
+--disable-setgid
+--disable-luit"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure () {
